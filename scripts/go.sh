71ffc7da13670bdde4eea2ccedea911ace3dac47
Florian Waltersdorfer <ignatiomobius@gmail.com>
2013-07-31 15:03:03 +0200

    added remove command

--- scripts/go.sh
+++ scripts/go.sh
@@ -1,10 +1,11 @@
 function go (){
   if [ $# == "0" ] || [ $1 == "-h" ]
   then
-    echo "Usage: go [(number)] [-l] [-a] [-h] (path)"
+    echo "Usage: go [(number)] [-l] [-a (path)] [-r (number)] [-h]"
     echo "    (number): cd to stored path with number"
     echo "    -l: list stored paths"
     echo "    -a: store new path to navigate"
+    echo "    -r: remove entry stored at given number"
     echo "    -h: show this help"
   elif [ $1 == "-a" ]
   then
@@ -16,6 +17,9 @@ function go (){
     do
       echo $line
     done <"$file"
+  elif [ $1 == "-r" ]
+  then
+    sed -i $2'd' /home/moebius/.go_history
   else
     i=1
     file="/home/moebius/.go_history"
